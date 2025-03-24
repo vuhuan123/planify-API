63681ee8f1eb839456d94f420d3ff871749166d6
Vupham <phamvanv15@gmail.com>
2025-03-24 15:51:06 +0700

    express boilerplate base

--- src/server.js
+++ src/server.js
@@ -1,13 +1,32 @@
-const express = require('express')
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+
+import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
+
 const app = express()
-const port = 8017;
+
 const hostname = 'localhost'
-app.get('/', function (req, res) {
-  res.send(`<h1>Hello Vux</h1>`)
+const port = 8017
+
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+    console.log(mapOrder(
+    [ { id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' } ],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello Vu</h1><hr>')
 })
 
-app.listen(port, hostname,()=>{
-    console.log(`App is running at http://${hostname}:${port}`);
-    
-
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`I am running at http://${ hostname }:${ port }/`)
 })
